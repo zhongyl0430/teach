e73b8eeebe2d07c4c6a47f27f11ffe33df5b633b
Luzuo Zhong <luzuo430@gmail.com>
2024-09-09 21:44:02 +0800

    Quartz sync: Sep 9, 2024, 9:44 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "",
+    baseUrl: "zhongyl0430.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,10 +7,10 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   afterBody: [],
   footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
+//    links: {
+//      GitHub: "https://github.com/jackyzha0/quartz",
+//      "Discord Community": "https://discord.gg/cRFFHYye7t",
+//    },
   }),
 }
 
