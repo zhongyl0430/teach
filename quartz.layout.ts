2aa8677b62073df05fa42c193bb055488786d994
zhongyl0430 <zhongyl0430@gmail.com>
2025-02-23 18:02:43 +0800

    Quartz sync: Feb 23, 2025, 6:02 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,13 +27,13 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
+    Component.Component.TableOfContents(),
+    Component.Backlinks(),
+    Component.DesktopOnly(Graph()),
 //    Component.DesktopOnly(Component.Explorer()),
   ],
-  right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
+//  right: [
+//  ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
