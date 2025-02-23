e42991ba68288012099ad8008c096342f6d6c22f
zhongyl0430 <zhongyl0430@gmail.com>
2025-02-23 23:42:22 +0800

    Quartz sync: Feb 23, 2025, 11:42 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -37,12 +37,16 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+//  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
+    Component.TableOfContents(),
+    Component.Backlinks(),
+    Component.DesktopOnly(Component.Graph()),
 //    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [],
